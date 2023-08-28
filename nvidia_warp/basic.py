ff26b348317a744f7fb5920c6f8f21db95f6c173
Hyojeong Yu <hyojeong.you@gmail.com>
2023-08-28 11:34:49 +0900

    Add: ground, cloth grid

--- nvidia_warp/basic.py
+++ nvidia_warp/basic.py
@@ -14,13 +14,14 @@ wp.init()
 
 class Example:
     def __init__(self, stage):
-        self.builder = wp.sim.ModelBuilder()
+        self.sim_fps = 60.0
+        self.sim_substeps = 64 # 업데이트 할 때 64번 서브스텝 진행 후 렌더링, 반복
+        self.sim_duration = 5.0
+        self.sim_frames = int(self.sim_duration * self.sim_fps)
+        self.sim_dt = (1.0 / self.sim_fps) / self.sim_substeps  # 초당 60Hz
 
-        self.sim_steps = 10
-        self.sim_dt = 1.0 / 60.0 # 초당 60Hz
         self.sim_time = 0.0 # 현재 시뮬레이션 시간, 렌더링 할 때 파라미터로 쓰임
 
-        self.sim_substeps = 64 # 업데이트 할 때 64번 서브스텝 진행 후 렌더링, 반복
         # self.sim_dt = self.frame_dt / self.sim_substeps
         # self.sim_steps = self.frame_count * self.sim_substeps
         
@@ -28,36 +29,40 @@ class Example:
 
 
     def create_scene(self, stage):
-        self.renderer = wp.render.UsdRenderer(stage)
+        builder = wp.sim.ModelBuilder()
 
         # ground
-        self.renderer.render_ground()
+        # builder.set_ground_plane()
         
         # cloth
         self.sim_width = 64
         self.sim_height = 32
 
-        self.builder.add_cloth_grid(
-                pos=(0.0, 4.0, 0.0),
-                rot=wp.quat_from_axis_angle((1.0, 0.0, 0.0), math.pi * 0.5),
-                vel=(0.0, 0.0, 0.0),
-                dim_x=self.sim_width,
-                dim_y=self.sim_height,
-                cell_x=0.1,
-                cell_y=0.1,
-                mass=0.1,
-                fix_left=True,
-                tri_ke=1.0e3,
-                tri_ka=1.0e3,
-                tri_kd=1.0e1,
-            )
+        builder.add_cloth_grid(
+            pos=(0.0, 4.0, 0.0),
+            rot=wp.quat_from_axis_angle((1.0, 0.0, 0.0), math.pi * 0.5),
+            vel=(0.0, 0.0, 0.0),
+            dim_x=self.sim_width,
+            dim_y=self.sim_height,
+            cell_x=0.1,
+            cell_y=0.1,
+            mass=0.1,
+            fix_left=True,
+            tri_ke=1.0e3,
+            tri_ka=1.0e3,
+            tri_kd=1.0e1,
+        )
         
-        self.model = self.builder.finalize(device="cuda", requires_grad=True)
+        self.model = builder.finalize(device="cuda", requires_grad=True)
+        self.model.ground = True
+
+        # self.renderer = wp.render.UsdRenderer(stage)
+        self.renderer = wp.sim.render.SimRenderer(self.model, stage, scaling=40.0)
+
+        self.integrator = wp.sim.SemiImplicitIntegrator()
 
         self.state_0 = self.model.state()
         self.state_1 = self.model.state()
-
-        self.integrator = wp.sim.SemiImplicitIntegrator()
 
 
     def update(self):
@@ -67,8 +72,7 @@ class Example:
             for s in range(self.sim_substeps):
                 self.state_0.clear_forces()
                 self.integrator.simulate(self.model, self.state_0, self.state_1, self.sim_dt)
-                # swap states
-                (self.state_0, self.state_1) = (self.state_1, self.state_0) 
+                (self.state_0, self.state_1) = (self.state_1, self.state_0) # swap states
 
 
     def render(self, is_live=False):
@@ -79,7 +83,7 @@ class Example:
             self.renderer.render(self.state_0)
             self.renderer.end_frame()
         
-        self.sim_time += self.sim_dt
+        self.sim_time += 1.0 / self.sim_fps
 
 
 
@@ -87,7 +91,7 @@ if __name__ == "__main__":
     stage_path = os.path.join(os.path.dirname(__file__), "outputs/basic.usd")
     example = Example(stage_path)
 
-    for i in range(example.sim_steps):
+    for i in range(example.sim_frames):
         example.update()
         example.render()
 
